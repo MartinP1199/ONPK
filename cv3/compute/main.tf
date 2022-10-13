8918412cb6291f408463f1c1ceac43eeb24cd8d8
MartinP1199 <potociarmartin@gmail.com>
2022-10-13 12:33:57 +0200

    http sec rule fix

--- cv3/compute/main.tf
+++ cv3/compute/main.tf
@@ -55,9 +55,11 @@ resource "openstack_networking_secgroup_rule_v2" "allow_public_http" {
   description       = "allow public http on 80"
   direction         = "ingress"
   ethertype         = "IPv4"
-  protocol          = "icmp"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = "${openstack_networking_secgroup_v2.secgroup_terrabuntu.id}"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_01.id}"
 }
 
 # --- keys ---
