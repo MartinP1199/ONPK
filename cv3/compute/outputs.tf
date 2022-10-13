cb0624e63494e825e89cf1586960998408aa21bc
MartinP1199 <potociarmartin@gmail.com>
2022-10-13 12:27:32 +0200

    added output

--- cv3/compute/outputs.tf
+++ cv3/compute/outputs.tf
@@ -1,3 +1,9 @@
-output "instance_ip_addr" {
+output "instance_name" {
   value = ["${openstack_compute_instance_v2.instance.*.name}"]
+  description = "Names of compute instances."
+}
+
+output "instance_ip_addr" {
+  value = ["${openstack_compute_instance_v2.instance.*.network.0.fixed_ip_v4}"]
+  description = "public IPs of compute instances."
 }
